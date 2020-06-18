f5df4f9c766f65ded3cf888be2ce9f8b83503f83
Vikram Choudhary <vikramus4@gmail.com>
2020-06-19 03:01:48 +0530

    Widget Css and content updated

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -3,32 +3,33 @@ import "./Snowflake.css";
 import "./Summer.css";
 import React from "react";
 import { WeatherCard, ForecastCardGrid } from "./WeatherCard";
-import { Summer, Winter } from "./WeatherBackground";
+import { Summer } from "./WeatherBackground";
 
-const seasonConfig = {
-  summer: {
-    text: "Let's hit the beach!",
-    iconName: "sun",
-    Component: "Summer",
-  },
-  winter: {
-    text: "Burr it is cold!",
-    iconName: "snowflakes",
-    Component: "Winter",
-  },
+const formatDate = () => {
+  let d = new Date(),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return [year, month, day].join("-");
 };
-
 const SeasonDisplay = (props) => {
   const { list, city } = { ...props.forecastData };
+  const forecast = list.filter((data) => {
+    if (!data.dt_txt.includes(formatDate()) && data.dt_txt.includes("12:00:00"))
+      return data;
+  });
+  const todaysFilteredData = list.filter((data) => {
+    if (data.dt_txt.includes(formatDate())) return data;
+  });
   return (
     <div>
       <Summer name={"sun"} />
-      <WeatherCard
-        date={new Date(parseInt(list && list[0].dt) * 1000).toDateString()}
-        cityName={city && city.name}
-        temp={list && list[0].main.temp}
-      />
-      <ForecastCardGrid weatherDetails={list} />
+      <WeatherCard cityName={city.name} todaysData={todaysFilteredData} />
+      <ForecastCardGrid weatherDetails={forecast} />
     </div>
   );
 };
